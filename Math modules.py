b7181dcc42a11b08154808a0b90add606ae449c0
Karthickj21 <jskarthickk@gmail.com>
2025-07-30 20:36:18 +0530

    Add files via upload

---
+++ Math modules.py
@@ -0,0 +1,10 @@
+import math
+n=float(input("Enter a number: "))
+
+sq=math.sqrt(n)
+log=math.log(n)
+sine=math.sin(n)
+
+print(f"Square root:{sq}")
+print(f"Logarithm:{log}")
+print(f"Sine:{sine}")
